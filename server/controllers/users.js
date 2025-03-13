6e0488ff4770eb26de6e4d7c89d691131e17cf05
Sydney TEXIER <sydney.texier@gmail.com>
2025-03-13 11:48:40 +0100

    Correction of the table "users", display of errors in request.http

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,18 +1,18 @@
 const User = require("../models/user");
 
-exports.post = async (req, res, next) => {
-    try {
-        const user = await User.create(req.body);
-        res.status(201).json(user);
-    } catch (error) {
-        next(error);
-    }
+exports.post = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    console.error("Erreur Sequelize :", err);
+    res.status(500).json({ error: err.message });
+  }
 };
 
-
 exports.getMe = async (req, res) => {
-    if (!req.user) {
-        return res.status(404).json({ error: "User not found" });
-    }
-    res.json(req.user);
+  if (!req.user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  res.json(req.user);
 };
--- server/models/user.js
+++ server/models/user.js
@@ -5,42 +5,43 @@ const connection = require("./db");
 class User extends Model {}
 
 User.init(
-    {
-        activated: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false,
-        },
-        fullName: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        email: {
-            type: DataTypes.STRING,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-        },
-        role: {
-            type: DataTypes.ENUM("USER", "ADMIN"),
-            defaultValue: "USER",
-        },
+  {
+    activated: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
-    {
-        sequelize: connection,
-    }
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+    role: {
+      type: DataTypes.ENUM("USER", "ADMIN"),
+      defaultValue: "USER",
+    },
+  },
+  {
+    tableName: "users",
+    sequelize: connection,
+  }
 );
 
 User.addHook("beforeCreate", async (user) => {
-    user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
+  user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
 });
 
 User.addHook("beforeUpdate", async (user, { fields }) => {
-    if (fields.includes("password")) {
-        user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
-    }
+  if (fields.includes("password")) {
+    user.password = await bcrypt.hash(user.password, await bcrypt.genSalt());
+  }
 });
 
 module.exports = User;
