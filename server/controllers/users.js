73950b420559f071c33a6e2de3638d7128a6f6bc
mickTa <mickael.talidec@edu.devinci.fr>
2025-03-19 21:41:50 +0100

    added a function to check users' passwords

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,14 +1,28 @@
 const User = require("../models/user");
 
+function validatePassword(password) {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return regex.test(password);
+}
+
+
 exports.post = async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
+      const { password } = req.body;
+
+      if (!validatePassword(password)) {
+          return res.status(400).json({
+              error: "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial."
+          });
+      }
+      const user = await User.create(req.body);
+      res.status(201).json(user);
   } catch (err) {
-    console.error("Erreur Sequelize :", err);
-    res.status(500).json({ error: err.message });
+      console.error("Erreur Sequelize :", err);
+      res.status(500).json({ error: err.message });
   }
 };
+
 
 exports.getMe = async (req, res) => {
   if (!req.user) {
