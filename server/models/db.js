d6dbbaee8f231c079dcba145d297223459b263a1
Sydney TEXIER <sydney.texier@gmail.com>
2025-03-20 13:14:03 +0100

    updating db.js #32

--- server/models/db.js
+++ server/models/db.js
@@ -1,17 +1,25 @@
+require("dotenv").config();
+
 const { Sequelize } = require("sequelize");
 
-const databaseUrl = "";
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  console.error("DATABASE_URL is not defined!");
+  process.exit(1);
+}
 
 const connection = new Sequelize(databaseUrl, {
-    dialect: "postgres",
-    logging: false,
+  dialect: "postgres",
+  logging: false,
 });
 
-connection.authenticate()
-    .then(() => console.log("Database connected!"))
-    .catch((err) => {
-        console.error("Database connection error:", err);
-        process.exit(1);
-    });
+connection
+  .authenticate()
+  .then(() => console.log("Database connected!"))
+  .catch((err) => {
+    console.error("Database connection error:", err);
+    process.exit(1);
+  });
 
 module.exports = connection;
