3f64738915d188470f4161b8981d3dbbf6efa1d3
jeremie-78 <154355057+jeremie-78@users.noreply.github.com>
2025-03-26 14:15:23 +0100

    added events table to db dump

--- Dumps/bdd_users_postgresql.sql
+++ Dumps/bdd_users_postgresql.sql
@@ -1,9 +1,10 @@
 -- PostgreSQL dump converted from MySQL
 
--- Drop table if it exists
+-- Reset tables
 DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "events";
 
--- Create table
+-- Initialize tables
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
   "activated" BOOLEAN DEFAULT FALSE,
@@ -17,10 +18,23 @@ CREATE TABLE "users" (
   "updatedAt" TIMESTAMP NOT NULL
 );
 
--- Insert data
+CREATE TABLE "events" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(255),
+  "organizer" VARCHAR(255),
+  "startDate" TIMESTAMP,
+  "endDate" TIMESTAMP,
+  "adress" VARCHAR(255)
+);
+
+-- Insert test data
 INSERT INTO "users" ("id", "activated", "fullName", "email", "password", "birthday", "role", "cguActivated", "createdAt", "updatedAt") 
 VALUES 
 (1, FALSE, 'John Doe', 'johndoe@example.com', '$2a$10$/FP1KvbK6LK15vdmPsAp5Omz6GSkaCTMhRhVUGvAwE67M7vCIfFI.', NULL, 'USER', FALSE, '2025-02-27 10:02:23', '2025-02-27 10:02:23');
+
+INSERT INTO "events" ("id", "name", "organizer", "startDate", "endDate", "adress") 
+VALUES 
+(1, "test", 'huiezhfiufez', '2025-06-15 12:30:00', '2025-06-15 12:40:00', 'somewhere');
 
 -- Ensure the sequence for SERIAL is correct
 SELECT setval(pg_get_serial_sequence('users', 'id'), COALESCE((SELECT MAX(id) FROM "users"), 1), false);
