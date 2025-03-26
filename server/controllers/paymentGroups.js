610c501421e5c614e3fb4aa72c2c87cfc4224152
bogogod <warmakeryt@gmail.com>
2025-03-26 18:07:55 +0100

    Creating model, router and controller for payment groups

---
+++ server/controllers/paymentGroups.js
@@ -0,0 +1,11 @@
+const PaymentGroup = require('../models/paymentGroup');
+
+exports.NewPaymentGroup = async (req, res)=>{
+  try {
+      const paymentGroup = await PaymentGroup.create(req.body);
+      res.status(201).json(paymentGroup);
+  } catch (err) {
+      console.error("Erreur Sequelize :", err);
+      res.status(500).json({ error: err.message });
+  }
+}
