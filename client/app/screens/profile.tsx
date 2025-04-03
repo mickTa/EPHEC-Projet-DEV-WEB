ca6a3772540377e0fe7292dab17e9a5b3d5bd00d
CharlieAubourg <charlie.aubourg@gmail.com>
2025-04-03 10:22:59 +0200

    finish last events bought section for profile page

--- client/app/screens/profile.tsx
+++ client/app/screens/profile.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, ActivityIndicator, StyleSheet, Alert } from "react-native";
 import EventContainer from "../components/EventContainer";
+import TabContainer from "../components/TabContainer";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
@@ -65,20 +66,40 @@ export default function ProfileScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.profilePic}></View>
-      <Text style={styles.title}>Bienvenue, {userData.fullName}!</Text>
-      <Text>Email: {userData.email}</Text>
-      <Text>Rôle: {userData.role}</Text>
-      <View>
-        <Text style={styles.title}>Vos derniers achats :</Text>
-        <EventContainer
-          title={"Event bought"}
-          text={"This is an event you bought"}
-        />
+    <>
+      <View style={styles.container}>
+        <View style={styles.profilePic}></View>
+        <Text style={styles.title}>Bienvenue, {userData.fullName}!</Text>
+        <Text>Email: {userData.email}</Text>
+        <Text>Rôle: {userData.role}</Text>
+        <View style={styles.events}>
+          <Text style={styles.title}>Vos derniers achats :</Text>
+          {/* insert for loop */}
+          <View style={styles.inlineEvents}>
+            <View style={styles.inlineEvent}>
+              <Text>Event #1 : Event name</Text>
+              <Text>0,00€</Text>
+            </View>
+            <View style={styles.inlineEvent}>
+              <Text>Event #2 : Event name</Text>
+              <Text>10,00€</Text>
+            </View>
+            <View style={styles.inlineEvent}>
+              <Text>Event #3 : Event name</Text>
+              <Text>50,00€</Text>
+            </View>    
+          </View>    
+        </View>
+        
       </View>
-      
-    </View>
+      {/*
+      <TabContainer
+          onPressEventTab1={goToWalletQR}
+          onPressEventTab2={goToProfile}
+          onPressEventTab3={goToProfile}
+      />
+      */}
+    </>
   );
 }
 
@@ -100,5 +121,25 @@ const styles = StyleSheet.create({
     borderRadius: "100%",
     backgroundColor: "lightgray",
     marginBottom: 40
+  },
+  events: {
+    marginTop: 40,
+    width: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inlineEvents: {
+    marginTop: 40,
+    flex: 1,
+    width: "100%",
+    flexDirection: "column",
+    //justifyContent: "space-between"
+  },
+  inlineEvent: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between"
   }
 });
