e17161692f724a5834edad0b6e83032ace64501d
bogogod <warmakeryt@gmail.com>
2025-04-14 11:25:44 +0200

    Deleting PaymentGroup model and references #54

--- server/routes/userPaymentGroupWallets.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const walletController = require('../controllers/userPaymentGroupWallets');
-
-
-const router = new Router();
-
-router.post('/', walletController.NewWallet);
-
-module.exports = router;
