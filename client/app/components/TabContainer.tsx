c83688357be7f55b2e145b250e7763d1c810848f
jeremie-78 <muchembled.jeremie78@gmail.com>
2025-04-24 14:37:22 +0200

    new navbar

--- client/app/components/TabContainer.tsx
+++ client/app/components/TabContainer.tsx
@@ -1,22 +1,49 @@
 import React from "react";
 import { View, StyleSheet, Image, Pressable } from "react-native";
+import {userDataFetcher} from "../misc/userDataFetcher";
+import{useState,useEffect}from"react";
+import { useRouter } from "expo-router";
 
-interface TabContainerProps {
-    onPressEventTab1: () => void;
-    onPressEventTab2: () => void;
-    onPressEventTab3: () => void;
-}
 
-const TabContainer: React.FC<TabContainerProps> = ({ onPressEventTab1, onPressEventTab2, onPressEventTab3 }) => {
+const router = useRouter();
+
+const goToProfile = () => {
+    router.replace("/screens/ProfileScreen");
+};
+
+const goToEvents = () => {
+    router.replace("/screens/EventFormScreen");
+};
+
+const goToWalletQR = () => {
+    router.replace("/screens/WalletQRCodeScreen");
+};
+
+const goToHome=()=>{
+    router.replace("/screens/HomeScreen");
+};
+
+const TabContainer: React.FC = () => {
+    const [buttonTwo,setButtonTwo]=useState(<></>);
+    useEffect(()=>{
+        const fetchData=async()=>{
+            const userData=await userDataFetcher();
+            if(userData&&userData.role=="ORGANIZER"){
+                setButtonTwo(<Pressable onPress={goToEvents}><Image source={require("../img/timetable-icon.png")} style={styles.icon} /></Pressable>)
+            }
+            if(userData&&userData.role=="USER"){
+                setButtonTwo(<Pressable onPress={goToWalletQR}><Image source={require("../img/timetable-icon.png")} style={styles.icon} /></Pressable>)
+            }
+        }
+        fetchData();
+    },[])
     return (
         <View style={styles.tabsBox}>
-            <Pressable onPress={onPressEventTab1}>
+            <Pressable onPress={goToHome}>
                 <Image source={require("../img/home-icon.png")} style={styles.icon} />
             </Pressable>
-            <Pressable onPress={onPressEventTab2}>
-                <Image source={require("../img/timetable-icon.png")} style={styles.icon} />
-            </Pressable>
-            <Pressable onPress={onPressEventTab3}>
+            {buttonTwo}
+            <Pressable onPress={goToProfile}>
                 <Image source={require("../img/profile-icon.png")} style={styles.icon} />
             </Pressable>
         </View>
