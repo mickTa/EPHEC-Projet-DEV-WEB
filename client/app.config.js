498a86f2d2fb929b249190f0e6f1db88ca5a61c9
Sydney TEXIER <sydney.texier@gmail.com>
2025-05-15 17:36:58 +0200

    cleaning up the architecture, removing nodes modules

--- client/app.config.js
+++ client/app.config.js
@@ -2,8 +2,12 @@ export default ({ config }) => {
   return {
     ...config,
     extra: {
+      ...config.expo?.extra,
       LOCALHOST_API: process.env.LOCALHOST_API,
       LAN_API: process.env.LAN_API,
+      eas: {
+        projectId: "51d82368-9ada-4f6d-8d18-e9eecbd3f93e",
+      },
     },
   };
 };
